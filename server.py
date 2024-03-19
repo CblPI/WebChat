1c451f5b963f27ea7244156098c47d4ad468078c
Ilya <85295023+CblPI@users.noreply.github.com>
2024-03-19 21:06:08 +0700

    рефакт

--- server.py
+++ server.py
@@ -24,33 +24,42 @@ class ChatServer:
         self.port = port
         self.clients = {}
 
-    async def handle_client(self, reader: StreamReader, writer):
+    async def handle_client(self, reader, writer):
         async for data in reader:
             message = data.decode()
             try:
+
                 message = json.loads(message)
+                print("message")
                 if message["event"] == "join":
                     self.clients[writer] = message["login"]
-                    await self.send_to_all({"event": "join", "login": message["login"]})
+                    await self.send_to_all(event="join", login= message["login"])
                     logging.info(f'Пользователь {message["login"]} присоединился.')
+
                 elif message["event"] == "message":
-                    await self.send_to_all({"event": "message", "text": message["text"], "user": self.clients[writer]})
+                    await self.send_to_all(event="message", text=message["text"], user= self.clients[writer])
                     logging.info(f'Сообщение от {self.clients[writer]}: {message["text"]}')
+
                 elif message["event"] == "leave":
+                    leaver = self.clients[writer]
                     del self.clients[writer]
-                    await self.send_to_all({"event": "leave", "login": message["login"]})
-                    logging.info(f'Пользователь: {message["login"]} покинул чат.')
-            except json.JSONDecodeError:
-                logging.error("Неверный JSON от клиента")
-        await self.send_to_all({"event": "leave", "login": self.clients[writer]})
-        logging.info(f'Пользователь: {self.clients[writer]} покинул чат.')
-        del self.clients[writer]
+                    await self.send_to_all(event="leave", login= message["login"])
+                    logging.info(f'Пользователь: {leaver} покинул чат.')
+
+            except json.JSONDecodeError: logging.error("Неверный JSON от клиента")
+
+        if writer in self.clients:
+            leaver = self.clients[writer]
+            await self.send_to_all(event= "leave", login=leaver)
+            del self.clients[writer]
+            logging.info(f'Пользователь: {leaver} покинул чат.')
 
 
-    async def send_to_all(self, message):
+    async def send_to_all(self, **kwargs):
         for client_writer in self.clients:
-            client_writer.write(json.dumps(message).encode() + b"\n")
+            client_writer.write(json.dumps(kwargs).encode() + b"\n")
             await client_writer.drain()
+
 
     async def run_server(self):
         server = await asyncio.start_server(
@@ -58,16 +67,15 @@ class ChatServer:
         )
         logging.info(f'Сервер запущен на: [{self.host}:{self.port}]')
 
-        async with server:
-            await server.serve_forever()
+        async with await server.serve_forever(): pass
 
 
 if __name__ == "__main__":
     port = os.getenv('PORT')
-    if (port is not None):
+    if (port):
         try:
             chat_server = ChatServer(port)
             asyncio.run(chat_server.run_server())
-        except KeyboardInterrupt:
-            logging.critical(f"Остановлено с {KeyboardInterrupt}")
+        except Exception as err:
+            logging.critical(f"Остановлено с {err}")
     else: logging.critical("Необходимо добавить значение порта в переменную окружения!")
